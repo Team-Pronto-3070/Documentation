6d7b0c26d65047ceb1fc9d3012ced97420f43d84
Cole <ricesarmagarfelos@gmail.com>
2015-02-07 10:59:06 -0800

    Started some explanation comments

--- src/usingEnumerationForFunctions/ExampleFunction.java
+++ src/usingEnumerationForFunctions/ExampleFunction.java
@@ -4,14 +4,38 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SpeedController;
 
 public class ExampleFunction {
+	/*
+	 * Even though this is sort of an enum, you create
+	 * it as a class
+	 * 
+	 * This is a State Machine. It keeps track of what
+	 * state the robot is as it performs a function.
+	 * The purpose of this is to simplify implementing 
+	 * functions for robot.
+	 */
 	
 	interface SpinningState {
 		public SpinningState check();
+		/*
+		 * Consider this a syntax thing and just copy it
+		 * For making a different State Machine, you would
+		 * replace the SpinningState with whatever function
+		 * you want to track the states of.
+		 * 
+		 * Ex: For a function that operates an elevator,
+		 * 
+		 * interface ElevatorState {
+		 * 	public ElevatorState check();
+		 * }
+		 */
 	}
 	
 	static Joystick xbox;
 	static SpeedController motor;
 	SpinningState state;
+	/*
+	 * 
+	 */
 	
 	public ExampleFunction(SpeedController m, Joystick x) {
 		motor = m;
