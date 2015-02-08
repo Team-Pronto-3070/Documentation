ee3aff46caadf1d7063160bdfe17a072fac0d543
Cole <ricesarmagarfelos@gmail.com>
2015-02-07 16:43:08 -0800

    Formatting

--- src/usingEnumerationForFunctions/ExampleFunction.java
+++ src/usingEnumerationForFunctions/ExampleFunction.java
@@ -5,57 +5,51 @@ import edu.wpi.first.wpilibj.SpeedController;
 
 public class ExampleFunction {
 	/*
-	 * Even though this is sort of an enum, you create
-	 * it as a class
+	 * Even though this is sort of an enum, you create it as a class
 	 * 
-	 * This is a State Machine. It keeps track of what
-	 * state the robot is as it performs a function.
-	 * The purpose of this is to simplify implementing 
+	 * This is a State Machine. It keeps track of what state the robot is as it
+	 * performs a function. The purpose of this is to simplify implementing
 	 * functions for robot.
 	 */
-	
+
 	interface SpinningState {
 		public SpinningState check();
 		/*
-		 * Consider this a syntax thing and just copy it
-		 * For making a different State Machine, you would
-		 * replace the SpinningState with whatever function
-		 * you want to track the states of.
+		 * Consider this a syntax thing and just copy it For making a different
+		 * State Machine, you would replace the SpinningState with whatever
+		 * function you want to track the states of.
 		 * 
 		 * Ex: For a function that operates an elevator,
 		 * 
-		 * interface ElevatorState {
-		 * 	public ElevatorState check();
-		 * }
+		 * interface ElevatorState { public ElevatorState check(); }
 		 */
 	}
-	
+
 	static Joystick xbox;
 	static SpeedController motor;
 	SpinningState state;
+
 	/*
-	 * Standard creation of objects in your class.
-	 * The term static is syntax. Just copy it.
-	 * If a non-static object is created that needs
-	 * to be static, Eclipse will give an error. That's
-	 * how you know what needs to be static.
+	 * Standard creation of objects in your class. The term static is syntax.
+	 * Just copy it. If a non-static object is created that needs to be static,
+	 * Eclipse will give an error. That's how you know what needs to be static.
 	 * 
-	 * state is what will keep track of the current state
-	 * you're in within the function.
+	 * state is what will keep track of the current state you're in within the
+	 * function.
 	 */
-	
+
 	public ExampleFunction(SpeedController m, Joystick x) {
 		// here's the Constructor
 		motor = m;
 		xbox = x;
 		// Assigning your class objects to the parameter objects
 		state = SpinningStates.Stopped;
-		/* 
-		 * This is what determines what the default state is
-		 * upon creating your State Machine in the main class.
+		/*
+		 * This is what determines what the default state is upon creating your
+		 * State Machine in the main class.
 		 */
 	}
-	
+
 	enum SpinningStates implements SpinningState {
 		// Stopped is a state
 		Stopped {
@@ -63,97 +57,89 @@ public class ExampleFunction {
 			@Override
 			public SpinningState check() {
 				/*
-				 * The check method will always be running
-				 * if you're running the State Machine.
-				 * However, the contents of check will vary
-				 * based on the state that you're in.
+				 * The check method will always be running if you're running the
+				 * State Machine. However, the contents of check will vary based
+				 * on the state that you're in.
 				 */
 				if (xbox.getRawButton(1)) {
 					return StartSpinning;
 					/*
-					 * Pressing the A Button will transition
-					 * to the StartSpinning state. The code
-					 * will no longer run the check method of
-					 * the Stopped state and will run the check
+					 * Pressing the A Button will transition to the
+					 * StartSpinning state. The code will no longer run the
+					 * check method of the Stopped state and will run the check
 					 * method of the StartSpinning state.
 					 */
 				}
-				
+
 				return Stopped;
 				/*
-				 * The check method has to return a SpinningState,
-				 * so until the A Button is pressed we want to stay
-				 * in the Stopped state.
+				 * The check method has to return a SpinningState, so until the
+				 * A Button is pressed we want to stay in the Stopped state.
 				 */
 			}
 		},
-		
+
 		StartSpinning {
 			@Override
 			public SpinningState check() {
 				spin();
 				/*
-				 * The spin method gets called, which sets
-				 * the motors, then transitions to the next
-				 * state: Spinning.
+				 * The spin method gets called, which sets the motors, then
+				 * transitions to the next state: Spinning.
 				 */
 				return Spinning;
 			}
 		},
-		
+
 		Spinning {
 			@Override
 			public SpinningState check() {
 				if (!xbox.getRawButton(1)) {
 					return StopSpinning;
 					/*
-					 * Until the A Button is released, the 
-					 * motor will remain set and the function
-					 * will remain in the Spinning state.
+					 * Until the A Button is released, the motor will remain set
+					 * and the function will remain in the Spinning state.
 					 */
 				}
-				
+
 				return Spinning;
 			}
 		},
-		
+
 		StopSpinning {
 			@Override
 			public SpinningState check() {
 				stop();
 				/*
-				 * Call the method that stops the motor,
-				 * then transitions to the next state.
+				 * Call the method that stops the motor, then transitions to the
+				 * next state.
 				 */
 				return Stopped;
 			}
 		}
 	}
-	
+
 	public void periodic() {
 		state = state.check();
 		/*
-		 * The periodic method will be called on your
-		 * State Machine in teleopPeriodic. This is
-		 * what determines what your function is doing
+		 * The periodic method will be called on your State Machine in
+		 * teleopPeriodic. This is what determines what your function is doing
 		 * depending on what state it's in.
 		 */
 	}
-	
+
 	public void stopPeriodic() {
 		stop();
 		/*
-		 * Set the motor to 0 if the robot is disabled
-		 * for obvious reasons.
+		 * Set the motor to 0 if the robot is disabled for obvious reasons.
 		 */
 	}
-	
+
 	private static void spin() {
 		motor.set(.5);
 		/*
-		 * Methods that perform the actual actions
-		 * for your Robot. These get called depending on
-		 * the state the State Machine is currently in.
+		 * Methods that perform the actual actions for your Robot. These get
+		 * called depending on the state the State Machine is currently in.
 		 */
 	}
 
--- src/usingEnumerationForFunctions/Main.java
+++ src/usingEnumerationForFunctions/Main.java
@@ -1,4 +1,3 @@
-
 package usingEnumerationForFunctions;
 
 import edu.wpi.first.wpilibj.CANTalon;
@@ -13,63 +12,62 @@ import edu.wpi.first.wpilibj.Joystick;
  * directory.
  */
 public class Main extends IterativeRobot {
-	
+
 	CANTalon motor;
 	Joystick xbox;
 	// Standard object creation
-	
+
 	ExampleFunction spinner;
+
 	/*
-	 * The explicit name of your class
-	 * followed by the label you will use
-	 * to refer to it in your main class, which
-	 * is this one.
+	 * The explicit name of your class followed by the label you will use to
+	 * refer to it in your main class, which is this one.
 	 */
-	
-    /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
-     */
-    public void robotInit() {
-    	motor = new CANTalon(1);
-    	xbox = new Joystick(1);
-    	
-    	spinner = new ExampleFunction(motor, xbox);
-    	/*
-    	 * You give your State Machine the parameters
-    	 * that were set up in the Constructor.
-    	 */
-    	
-    }
 
-    /**
-     * This function is called periodically during autonomous
-     */
-    public void autonomousPeriodic() {
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	public void robotInit() {
+		motor = new CANTalon(1);
+		xbox = new Joystick(1);
 
-    }
+		spinner = new ExampleFunction(motor, xbox);
+		/*
+		 * You give your State Machine the parameters that were set up in the
+		 * Constructor.
+		 */
 
-    /**
-     * This function is called periodically during operator control
-     */
-    public void teleopPeriodic() {
-        spinner.periodic();
-        /*
-         * This is what operates the check method
-         * of the current state that spinner is in.
-         */
-    }
-    
-    public void disabledInit() {
-    	spinner.stopPeriodic();
-    	// Stops the motor.
-    }
-    
-    /**
-     * This function is called periodically during test mode
-     */
-    public void testPeriodic() {
-    
-    }
-    
+	}
+
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	public void autonomousPeriodic() {
+
+	}
+
+	/**
+	 * This function is called periodically during operator control
+	 */
+	public void teleopPeriodic() {
+		spinner.periodic();
+		/*
+		 * This is what operates the check method of the current state that
+		 * spinner is in.
+		 */
+	}
+
+	public void disabledInit() {
+		spinner.stopPeriodic();
+		// Stops the motor.
+	}
+
+	/**
+	 * This function is called periodically during test mode
+	 */
+	public void testPeriodic() {
+
+	}
+
 }
